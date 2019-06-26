7b24ae775bed9d599f90c01268937f59193d5915
Bill Bliss <billbl@microsoft.com>
2019-06-26 09:06:04 -0700

    Add build step to deployment script

--- deploy.sh
+++ deploy.sh
@@ -109,11 +109,13 @@ fi
 # 2. Select node version
 selectNodeVersion
 
-# 3. Install npm packages
+# 3. Install npm packages and do a build
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   echo "Running $NPM_CMD install --production"
   eval $NPM_CMD install --production
+  echo "Running $NPM_CMD run-script build"
+  eval $NPM_CMD run-script build
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
